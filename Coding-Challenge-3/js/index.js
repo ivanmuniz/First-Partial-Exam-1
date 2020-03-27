6bc085f1a9cd2b76118420c7faf0ff7a1d4de92e
Iván Muñiz <ivanmr_96@live.com.mx>
2020-03-27 09:56:42 -0600

    Avance ultimo problema

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,35 @@
+function game() {
+    // results.innerHTML = "";
+    let input = Number(document.querySelector(".fizzBuzzNumber").value);
+    let results = document.querySelector(".resultsSection");
+    for(let i = 1 ; i <= input; i++) {
+        if( i % 3 === 0 ) {
+            results.innerHTML += `<div class="fizz-buzz-item fizz"><span>fizz</span</div>`
+        } else if ( i % 5 === 0) {
+            results.innerHTML += `<div class="fizz-buzz-item buzz"><span>buzz</span></div>`
+        } else {
+            results.innerHTML += `<div class="fizz-buzz-item"><span>${i}</span></div>`
+        }
+    }
+
+}
+
+function watchForm() {
+    let form = document.getElementById("fizzBuzzForm");
+    form.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+    });
+}
+
+function watchSubmit() {
+    let submit = document.getElementById("submitBtn");
+    submit.addEventListener("click", game);
+}
+
+
+function init() {
+    watchForm();
+    watchSubmit();
+}
+
+init()
